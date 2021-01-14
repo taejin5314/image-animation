c9b95b95431a74bf33f7880ac2401facbc0cdc63
taejin <taejin5314@gmail.com>
2021-01-14 17:07:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,4 +9,20 @@ myImage.addEventListener('load', function () {
     canvas.height = 700;
 
     ctx.drawImage(myImage, 0, 0, canvas.width, canvas.height);
+
+    let particlesArray = [];
+    const numberOfParticles = 5000;
+
+    class Particle {
+        constructor() {
+            this.x = Math.random() * canvas.width;
+            this.y = 0;
+            this.speed = 0;
+            this.velocity = Math.random() * 0.5;
+            this.size = Math.random() * 1.5 + 1;
+        }
+        update() {
+
+        }
+    }
 }, false)
