2c65f35dbcf7609a21d8888eef7980adbd40a0d4
taejin <taejin5314@gmail.com>
2021-01-14 17:01:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 const myImage = new Image();
-myImage.src = '';
+myImage.src = 'C:\Users\taeji\Documents\GitHub\image-animation\witcher.jpg';
+
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = 500;
+canvas.height = 706;
+
+ctx.drawImage(myImage, 0, 0, canvas.width, canvas.height);
