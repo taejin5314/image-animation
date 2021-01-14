cad3ce8ef45b234af279c08c2d846ebfff267aee
taejin <taejin5314@gmail.com>
2021-01-14 17:37:31 -0500

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,22 @@ myImage.addEventListener('load', function () {
             const red = pixels.data[(y * 4 * pixels.width) + (x * 4)];
             const green = pixels.data[(y * 4 * pixels.width) + (x * 4 + 1)];
             const blue = pixels.data[(y * 4 * pixels.width) + (x * 4 + 2)];
+            const brightness = calculateRelativeBrightness(red, green, blue);
+            const cell = [
+                brightness,
+            ]
+            row.push(cell);
         }
+        mappedImage.push(row);
+    }
+    console.log(mappedImage)
+
+    function calculateRelativeBrightness(red, green, blue) {
+        return Math.sqrt(
+            (red * red) * 0.299 +
+            (green * green) * 0.587 +
+            (blue * blue) * 0.114
+        ) / 100;
     }
 
     class Particle {
@@ -30,11 +45,18 @@ myImage.addEventListener('load', function () {
             this.x = Math.random() * canvas.width;
             this.y = 0;
             this.speed = 0;
-            this.velocity = Math.random() * 2.5;
+            this.velocity = Math.random() * 1.5;
             this.size = Math.random() * 1.5 + 1;
+            this.position1 = Math.floor(this.y);
+            this.position2 = Math.floor(this.x);
         }
         update() {
-            this.y += this.velocity;
+            this.position1 = Math.floor(this.y);
+            this.position2 = Math.floor(this.x);
+            this.speed = mappedImage[this.position1][this.position2][0];
+            let movement = (2.5 - this.speed) + this.velocity;
+
+            this.y += movement;
             if (this.y >= canvas.height) {
                 this.y = 0;
                 this.x = Math.random() * canvas.width;
@@ -56,7 +78,7 @@ myImage.addEventListener('load', function () {
 
     init();
     function animate() {
-        ctx.drawImage(myImage, 0, 0, canvas.width, canvas.height);
+        // ctx.drawImage(myImage, 0, 0, canvas.width, canvas.height);
         ctx.globalAlpha = 0.05;
         ctx.fillStyle = 'rgb(0, 0, 0)';
         ctx.fillRect(0, 0, canvas.width, canvas.height);
