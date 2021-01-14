a9896229048801c3df52763fa463737a8fbb9900
taejin <taejin5314@gmail.com>
2021-01-14 17:05:36 -0500

    update

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const myImage = new Image();
-myImage.src = 'C:\Users\taeji\Documents\GitHub\image-animation\witcher.jpg';
+myImage.src = 'witcher.jpg';
 
-const canvas = document.getElementById('canvas1');
-const ctx = canvas.getContext('2d');
-canvas.width = 500;
-canvas.height = 706;
 
-ctx.drawImage(myImage, 0, 0, canvas.width, canvas.height);
+myImage.addEventListener('load', function () {
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 700;
+    canvas.height = 700;
+
+    ctx.drawImage(myImage, 0, 0, canvas.width, canvas.height);
+}, false)
