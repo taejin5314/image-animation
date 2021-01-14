1cdff219acc8d4bf6dc488acb6336ed30dc00fe5
taejin <taejin5314@gmail.com>
2021-01-14 17:15:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -18,11 +18,42 @@ myImage.addEventListener('load', function () {
             this.x = Math.random() * canvas.width;
             this.y = 0;
             this.speed = 0;
-            this.velocity = Math.random() * 0.5;
+            this.velocity = Math.random() * 2.5;
             this.size = Math.random() * 1.5 + 1;
         }
         update() {
-
+            this.y += this.velocity;
+            if (this.y >= canvas.height) {
+                this.y = 0;
+                this.x = Math.random() * canvas.width;
+            }
+        }
+        draw() {
+            ctx.beginPath();
+            ctx.fillStyle = 'white';
+            ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+            ctx.fill();
         }
     }
+
+    function init() {
+        for (let i = 0; i < numberOfParticles; i++) {
+            particlesArray.push(new Particle);
+        }
+    }
+
+    init();
+    function animate() {
+        ctx.drawImage(myImage, 0, 0, canvas.width, canvas.height);
+        ctx.globalAlpha = 0.05;
+        ctx.fillStyle = 'rgb(0, 0, 0)';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        for (let i = 0; i < particlesArray.length; i++) {
+            particlesArray[i].update();
+            particlesArray[i].draw();
+        }
+        requestAnimationFrame(animate);
+    }
+
+    animate();
 }, false)
